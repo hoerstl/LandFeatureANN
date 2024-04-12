3835a2c213c3d4a0f80c80109c6fb5289d4c0c4f
hoerstl <hoerstl@berea.edu>
2024-04-12 14:19:41 -0400

    Updated the cropper and scaler to work with more than one folder

--- theScaler.py
+++ theScaler.py
@@ -10,21 +10,34 @@ def scaleImage(filepath, scalingDimensions):
 
 
 
-
-def main():
-    fileNames = [filename for filename in os.listdir("./cropped")]
-    unscaledImagePaths = [f"cropped/{filename}" for filename in fileNames]
-    scaledImagePaths = [f"scaled/{filename}" for filename in fileNames]
+def scaleImages(rawImageFolderName, outputFolderName):
+    fileNames = [filename for filename in os.listdir(f"./{rawImageFolderName}")]
+    unscaledImagePaths = [f"{rawImageFolderName}/{filename}" for filename in fileNames]
+    scaledImagePaths = [f"{outputFolderName}/{filename}" for filename in fileNames]
 
     scalingDimensions = (200, 200)
 
     for filename, inputPath, outputPath in zip(fileNames, unscaledImagePaths, scaledImagePaths):
         print(f"Scaling img '{filename}' to size {scalingDimensions}. ", end="")
         scaledImg = scaleImage(inputPath, scalingDimensions)
-        # use img.save(filepath) to save an image
         scaledImg.save(outputPath)
         print(f"Save successful")
         scaledImg.close()
+
+
+
+def main():
+    # Scaling the training images
+    scaleImages("training_cropped_images", "training_scaled_images")
+    
+    # Scaling the testing images
+    scaleImages("testing_cropped_images", "testing_scaled_images")
+    
+    # Scale the colored testing images
+    scaleImages("training_colored_images", "training_colored_scaled_images")
+    
+    # Scale the colored training images
+    scaleImages("testing_colored_cropped_images", "testing_colored_scaled_images")
 
 
 if __name__ == "__main__":
